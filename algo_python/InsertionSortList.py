0b8bb2149beba492c363fb534ddc7e66d598e8b0
methney <mindcure@gmail.com>
2022-02-23 19:39:55 +0900

    insertionSortList

--- algo_python/InsertionSortList.py
+++ algo_python/InsertionSortList.py
@@ -17,12 +17,20 @@ class Solution:
             while cur.next and cur.next.val < head.val:
                 cur = cur.next
             cur.next, head.next, head = head, cur.next, head.next
+            
+            # 첫번째---------------------------------
             # cur.next = head 하면, None -> 4가 붙는다고 생각하겠지만, 사실은 None->4 뒤로 2->1->3 다 따라붙는다. (이게핵심) head는 비어있게되나?
             # 일단 두번째 while안에서 cur = cur.next 실행(만족하면)
             # head.next = cur.next를 하면, 4-2-1-3(cur.next) 이거를 head.next (4뒤에)
             # 그럼 cur에 None-4, head에는 4-2-1-3
             # 그리고 다시 head = head.next를 통해서 2-1-3이 된다. 
+            # 결국 첫번째 사이클이 끝나고 None-4(cur), 2-1-3(head) 가 된다.
+            
+            # 두번째 -----------------------------------
+            # 4 < 2 이어서 두번째 while비실행
+            # 
 
+            # cur를 다시 맨처음 None으로 초기화
             cur = parent
 
         return cur.next
