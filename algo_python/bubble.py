1d8621a5b6d53c9ef9c230215d91345dbddd1069
ghlee <ghlee@dlab.kr>
2022-02-23 19:42:41 +0900

    bubble

--- algo_python/bubble.py
+++ algo_python/bubble.py
@@ -14,24 +14,24 @@ def printVal(arr):
 def bubbleSort(arr):
     l = end = len(arr)
     # How1.
-    # for i in range(l-1,0,-1):    
-    #     for j in range(i):
-    #         if arr[j] > arr[j+1] : 
-    #             arr[j], arr[j+1] = arr[j+1], arr[j]
+    for i in range(l-1,0,-1):    
+        for j in range(i):
+            if arr[j] > arr[j+1] : 
+                arr[j], arr[j+1] = arr[j+1], arr[j]
 
     # How2.
-    while end > 0 :
-        swap = False
-        for i in range(end-1):
-            if arr[i] > arr[i+1]:
-                arr[i], arr[i+1] = arr[i+1], arr[i]
-                swap = True
-        if not swap :
-            break 
-        end -= 1
+    # while end > 0 :
+    #     swap = False
+    #     for i in range(end-1):
+    #         if arr[i] > arr[i+1]:
+    #             arr[i], arr[i+1] = arr[i+1], arr[i]
+    #             swap = True
+    #     if not swap :
+    #         break 
+    #     end -= 1
     
     return arr
 
-# bubbleSort(arr)
+bubbleSort(arr)
 # printVal(arr)
 
